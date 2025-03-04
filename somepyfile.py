ac044a1498598cd37955cd03875a2d0f781cfe2b
simon <semyon.vladimirov.06@gmail.com>
2025-03-04 15:44:02 +0300

    class user

--- somepyfile.py
+++ somepyfile.py
@@ -1,3 +1,15 @@
-print("this is just a py file")
-for i in range(5):
-    print(i + 1)
+class User:
+    def __init__(self, userID: int, name: str, age: int):
+        self.userID = userID
+        self.name = name
+        self.age = age
+
+    def Display(self):
+        print("--------------------")
+        print(f"user id: {self.userID}")
+        print(f"user age: {self.age}")
+        print(f"user name: {self.name}")
+        print("--------------------")
+
+tobi = User(22, 'Simon', 18)
+tobi.Display()
